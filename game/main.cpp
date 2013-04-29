a0729d9f80d91936ab2788cffe224ce247a7ad73
Vaibhav Gupta <kacxdak@gmail.com>
2013-04-29 09:58:03 -0500

    Added basic game playing

---
+++ game/main.cpp
@@ -0,0 +1,73 @@
+#include <stdlib.h>
+#include <cstdlib>
+#include <time.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+
+#include "main.h"
+#include "game.h"
+//#include "agent.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {	
+	srand ( time(NULL) );
+
+	int gameCounter = 1;
+	bool debug = false;
+	string dumpfile = "data";
+	//read arguments and define variable based on them
+	for(int i=1; i<argc; i++) {
+		string temp = argv[i];
+		if (temp.compare("-debug") == 0) {
+			debug = true;
+		} else if (temp.compare("-g") == 0) {
+			if(++i<argc)
+				gameCounter = atoi(argv[i]);
+			else
+				usage_err(temp);
+		} else if (temp.compare("-dump") == 0) {
+			if(++i<argc)
+				dumpfile = argv[i];
+			else
+				usage_err(temp);
+		} else
+			usage_err(temp);
+	}
+	Game *game = new Game();
+	//game->setDebug(debug);
+	//Agent *agent = new Agent(game);
+	//agent->setDebug(debug);
+
+	for(int i=0; i<gameCounter; i++) {
+		if(debug) cout<<"Running Trial: "<<i<<endl;
+		game->Reset();
+		while(!game->Status()) {
+			game->Print();
+			//game->Eval();
+			//Action move = agent->Move();
+			//if(debug) cout<<"\tTaking Action: "<<move<<endl;
+			cout<<"Move: ";
+			int move;
+			cin>>move;
+			game->Move(move, false);
+		}
+		//agent->Update();
+	}
+	//agent->DumpPolicy(dumpfile);
+	return 0;
+}
+
+void usage_err(string key) {
+	if(key.compare("-h") != 0)
+		cout<<"Improper usage of "<<key<<endl;
+	cout<<"Usage: ./tic-tac-toe [-g episode] [-dump file] [-debug]"<<endl;
+	cout<<"./tic-tac-toe -h displays usage"<<endl;
+	cout<<"View README for info"<<endl;
+	exit(0);
+}
